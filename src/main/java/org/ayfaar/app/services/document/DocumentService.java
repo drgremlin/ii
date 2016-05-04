011b12a62cb5ac5112773adf78e8f9b14deca597
Den Street <masloed@yandex.ru>
2016-05-04 21:30:42 +0300

    Autocomplit add topics,categories,documents,video_resources Autocomplit fix

--- src/main/java/org/ayfaar/app/services/document/DocumentService.java
+++ src/main/java/org/ayfaar/app/services/document/DocumentService.java
@@ -4,5 +4,6 @@ import java.util.Map;
 
 public interface DocumentService {
 
+    public void reload();
     Map<String, String> getAllUriNames();
 }
--- src/main/java/org/ayfaar/app/services/videoResource/VideoResourceService.java
+++ src/main/java/org/ayfaar/app/services/videoResource/VideoResourceService.java
@@ -3,5 +3,6 @@ package org.ayfaar.app.services.videoResource;
 import java.util.Map;
 
 public interface VideoResourceService {
+    public void reload();
     Map<String, String> getAllUriNames();
 }
