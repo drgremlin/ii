b6f8516a8f4a1c047c73df54a4e5a7b1ddf0dae2
Den Street <masloed@yandex.ru>
2016-09-21 22:29:34 +0300

    downloads Documents to GDrive, DocumentController

--- src/main/java/org/ayfaar/app/controllers/DocumentController.java
+++ src/main/java/org/ayfaar/app/controllers/DocumentController.java
@@ -1,7 +1,9 @@
 package org.ayfaar.app.controllers;
 
+import com.google.api.services.drive.model.File;
 import org.ayfaar.app.dao.CommonDao;
 import org.ayfaar.app.model.Document;
+import org.ayfaar.app.model.Image;
 import org.ayfaar.app.utils.GoogleService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +16,7 @@ import java.util.Optional;
 
 import static org.ayfaar.app.utils.UriGenerator.generate;
 import static org.springframework.data.domain.Sort.Direction.DESC;
+import static org.springframework.util.Assert.hasLength;
 
 @RestController
 @RequestMapping("api/document")
@@ -32,15 +35,34 @@ public class DocumentController {
         return commonDao.getOpt(Document.class, generate(Document.class, docId))
             .orElseGet(() -> {
                 final GoogleService.DocInfo docInfo = googleService.getDocInfo(docId);
+                String docName = name.orElse(docInfo.title);
+                String currentDocId = docId;
+                String currentUrl = docInfo.downloadUrl;
+                String mimeType = docInfo.mimeType;
+                String thumbnailLink = docInfo.thumbnailLink;
+                String iconLink = docInfo.iconLink;
+
+                if(!url.contains("google.com")){
+                    try {
+                        File file = googleService.uploadToGoogleDrive(url, docName);
+                        currentDocId = file.getId();
+                        currentUrl = file.getAlternateLink();
+                        mimeType = file.getMimeType();
+                        thumbnailLink = file.getThumbnailLink();
+                        iconLink = file.getIconLink();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
                 final Document document = Document.builder()
-                        .id(docId)
-                        .name(name.orElse(docInfo.title))
+                        .id(currentDocId)
+                        .name(docName)
                         .annotation(annotation)
                         .author(author)
-                        .thumbnail(docInfo.thumbnailLink)
-                        .mimeType(docInfo.mimeType)
-                        .icon(docInfo.iconLink)
-                        .downloadUrl(docInfo.downloadUrl)
+                        .thumbnail(thumbnailLink)
+                        .mimeType(mimeType)
+                        .icon(iconLink)
+                        .downloadUrl(currentUrl)
                         .build();
                 return commonDao.save(document);
             });
@@ -55,5 +77,15 @@ public class DocumentController {
     @RequestMapping("last")
     public List<Document> getLast(@PageableDefault(size = 9, sort = "createdAt", direction = DESC) Pageable pageable) {
         return commonDao.getPage(Document.class, pageable);
+    }
+
+    @RequestMapping(value = "update-name", method = RequestMethod.POST)
+    public void updateTitle(@RequestParam String uri, @RequestParam String title) {
+        hasLength(uri);
+       Document document = commonDao.get(Document.class, "uri", uri);
+        if (document != null) {
+            document.setName(title);
+            commonDao.save(document);
+        }
     }
 }
