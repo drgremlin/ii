76a0303c71244517688315ded0715c40aec39396
Den Street <masloed@yandex.ru>
2016-09-22 15:19:05 +0300

    upload images to GDrive

--- src/main/java/org/ayfaar/app/controllers/DocumentController.java
+++ src/main/java/org/ayfaar/app/controllers/DocumentController.java
@@ -1,9 +1,9 @@
 package org.ayfaar.app.controllers;
 
 import com.google.api.services.drive.model.File;
+import lombok.extern.slf4j.Slf4j;
 import org.ayfaar.app.dao.CommonDao;
 import org.ayfaar.app.model.Document;
-import org.ayfaar.app.model.Image;
 import org.ayfaar.app.utils.GoogleService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -18,6 +18,7 @@ import static org.ayfaar.app.utils.UriGenerator.generate;
 import static org.springframework.data.domain.Sort.Direction.DESC;
 import static org.springframework.util.Assert.hasLength;
 
+@Slf4j
 @RestController
 @RequestMapping("api/document")
 //todo: rename, update author etc
@@ -31,38 +32,27 @@ public class DocumentController {
                            @RequestParam(required = false) String author,
                            @RequestParam(required = false) String annotation) {
         Assert.hasLength(url);
+        if(!url.contains("google.com")){
+            try {
+                File file = googleService.uploadToGoogleDrive(url, name.orElse("doc"));
+                url = file.getAlternateLink();
+            } catch (Exception e) {
+               log.error(e.getMessage());
+            }
+        }
         final String docId = GoogleService.extractDocIdFromUrl(url);
         return commonDao.getOpt(Document.class, generate(Document.class, docId))
             .orElseGet(() -> {
                 final GoogleService.DocInfo docInfo = googleService.getDocInfo(docId);
-                String docName = name.orElse(docInfo.title);
-                String currentDocId = docId;
-                String currentUrl = docInfo.downloadUrl;
-                String mimeType = docInfo.mimeType;
-                String thumbnailLink = docInfo.thumbnailLink;
-                String iconLink = docInfo.iconLink;
-
-                if(!url.contains("google.com")){
-                    try {
-                        File file = googleService.uploadToGoogleDrive(url, docName);
-                        currentDocId = file.getId();
-                        currentUrl = file.getAlternateLink();
-                        mimeType = file.getMimeType();
-                        thumbnailLink = file.getThumbnailLink();
-                        iconLink = file.getIconLink();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
                 final Document document = Document.builder()
-                        .id(currentDocId)
-                        .name(docName)
+                        .id(docId)
+                        .name(name.orElse(docInfo.title))
                         .annotation(annotation)
                         .author(author)
-                        .thumbnail(thumbnailLink)
-                        .mimeType(mimeType)
-                        .icon(iconLink)
-                        .downloadUrl(currentUrl)
+                        .thumbnail(docInfo.thumbnailLink)
+                        .mimeType(docInfo.mimeType)
+                        .icon(docInfo.iconLink)
+                        .downloadUrl(docInfo.downloadUrl)
                         .build();
                 return commonDao.save(document);
             });
@@ -82,10 +72,10 @@ public class DocumentController {
     @RequestMapping(value = "update-name", method = RequestMethod.POST)
     public void updateTitle(@RequestParam String uri, @RequestParam String title) {
         hasLength(uri);
-       Document document = commonDao.get(Document.class, "uri", uri);
-        if (document != null) {
-            document.setName(title);
-            commonDao.save(document);
-        }
+        Optional<Document> optDoc = commonDao.getOpt(Document.class, "uri", uri);
+        Document document = optDoc.get();
+        document.setName(title);
+        commonDao.save(document);
+
     }
 }
